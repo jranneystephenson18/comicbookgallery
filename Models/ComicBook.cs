8a5e008f875834f6af942b2a7c139f663f95db59
Joy Stephenson <j.ranney.stephenson@gmail.com>
2018-11-08 19:06:23 -0500

    adding files

---
+++ Models/ComicBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comic_book_gallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionalHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " # " + IssueNumber;
+            }
+        }
+            // series-title-issuenumber.jpg
+
+            public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+        }
+    }
